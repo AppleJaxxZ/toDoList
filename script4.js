d77410a9e56598af4d4796628d73a0e0f1de39a4
AppleJaxxZ <65720484+AppleJaxxZ@users.noreply.github.com>
2020-11-22 14:17:26 -0500

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,28 @@
+let addToButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+let removeButton = document.getElementById('remove');
+
+addToButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through";
+    });
+
+    removeButton.addEventListener('click', function() {
+        toDoContainer.removeChild(paragraph);
+    });
+
+});
+
+inputField.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        document.getElementById('addToDo').click();
+    }
+})
+
