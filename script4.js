dde4bcf718b1f04fa6e1854db37110fd2a39f03e
AppleJaxxZ <65720484+AppleJaxxZ@users.noreply.github.com>
2020-11-24 22:19:17 -0500

    Update script4.js
    changed it back because this works BUT it still gives me a script error.  still trying to figure this out.

--- script4.js
+++ script4.js
@@ -14,7 +14,7 @@ addToButton.addEventListener('click', function(){
     });
 
     removeButton.addEventListener('click', function() {
-        paragraph.foreach(para => toDoContainer.removeChild(para));
+        toDoContainer.removeChild(paragraph)
     });
 
 });
