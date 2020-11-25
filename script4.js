ff046a04d3e23bc8d50de755132e76e8444f40bb
Kerosz <andrei_chirila@outlook.com>
2020-11-25 11:36:38 +0200

    refactor files

--- script4.js
+++ script4.js
@@ -1,28 +1,71 @@
-let addToButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
-let removeButton = document.getElementById('remove');
+const toDoApp = () => {
+  //  DOM selectors
+  const addToButton = document.getElementById('addToDo');
+  const toDoContainer = document.getElementById('toDoContainer');
+  const clearButton = document.getElementById('remove');
+  const inputField = document.getElementById('inputField');
 
-addToButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click', function() {
-        paragraph.style.textDecoration = "line-through";
-    });
-
-    removeButton.addEventListener('click', function() {
-        toDoContainer.removeChild(paragraph)
-    });
-
-});
-
-inputField.addEventListener('keyup', function(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        document.getElementById('addToDo').click();
+  //  Helper functions
+  const clearToDo = () => {
+    while (toDoContainer.firstChild) {
+      toDoContainer.removeChild(toDoContainer.firstChild);
     }
-})
+  };
 
+  const toggleToDo = (todo) => {
+    todo.classList.toggle('completed');
+  };
+
+  const removeToDo = (todo) => {
+    todo.remove();
+  };
+
+  const addToDo = () => {
+    if (inputField.value !== '') {
+      // Creates the ToDo
+      const paragraph = document.createElement('p');
+      paragraph.classList.add('paragraph-styling');
+      paragraph.innerText = inputField.value;
+
+      // Crates buttons container
+      const buttonContainer = document.createElement('div');
+
+      // Creates the toggle button
+      const toggleBtn = document.createElement('button');
+      toggleBtn.className = 'toggle-btn';
+      toggleBtn.textContent = 'Check';
+
+      // Creates the remove button
+      const removeBtn = document.createElement('button');
+      removeBtn.className = 'remove-btn';
+      removeBtn.textContent = 'Remove';
+
+      // Adds the elements to DOM
+      buttonContainer.appendChild(toggleBtn);
+      buttonContainer.appendChild(removeBtn);
+      paragraph.appendChild(buttonContainer);
+      toDoContainer.appendChild(paragraph);
+
+      // Listners for our created elements
+      toggleBtn.addEventListener('click', function () {
+        toggleToDo(this.parentElement.parentElement);
+      });
+      removeBtn.addEventListener('click', function () {
+        removeToDo(this.parentElement.parentElement);
+      });
+    }
+
+    // Resets the input text
+    inputField.value = '';
+  };
+
+  // Listners for adding and clearing the todos
+  addToButton.addEventListener('click', addToDo);
+  inputField.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') addToDo();
+  });
+  clearButton.addEventListener('click', clearToDo);
+};
+
+// Starts your application on window load
+window.onload = toDoApp;
